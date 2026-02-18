d0a8db75d39baf219263c609fbdddae8194be5e7
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-02-18 13:59:27 +0100

    Add mod uninstall confirmation

--- launcher/modManager/cmodlistview_moc.cpp
+++ launcher/modManager/cmodlistview_moc.cpp
@@ -1120,7 +1120,7 @@ void CModListView::installMods(QStringList archives)
 			if (modStateModel->isModEnabled(mod))
 				modsToEnable.push_back(mod);
 
-			manager->uninstallMod(mod);
+			doUninstallMod(mod, true);
 			reload(mod);
 		}
 		else
@@ -1407,15 +1407,22 @@ void CModListView::doInstallMod(const QString & modName)
 	}
 }
 
-void CModListView::doUninstallMod(const QString & modName)
+void CModListView::doUninstallMod(const QString & modName, bool silent)
 {
-	if(modStateModel->isModExists(modName) && modStateModel->getMod(modName).isInstalled())
+	if(!modStateModel->isModExists(modName) || !modStateModel->getMod(modName).isInstalled())
+		return;
+
+	if(!silent)
 	{
-		if(modStateModel->isModEnabled(modName))
-			manager->disableMod(modName);
-		manager->uninstallMod(modName);
-		reload(modName);
+		int result = QMessageBox::question(this, tr("Uninstall mod"), tr("Are you sure you want to uninstall %1?").arg(modStateModel->getMod(modName).getName()), QMessageBox::Yes | QMessageBox::No, QMessageBox::No);
+		if(result != QMessageBox::Yes)
+			return;
 	}
+
+	if(modStateModel->isModEnabled(modName))
+		manager->disableMod(modName);
+	manager->uninstallMod(modName);
+	reload(modName);
 }
 
 bool CModListView::isModAvailable(const QString & modName)
