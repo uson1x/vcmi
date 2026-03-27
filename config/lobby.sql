352f47b79cdd720fc5dc8cc5346f06ec02092267
Ivan Savenko <saven.ivan@gmail.com>
2026-03-27 21:52:15 +0200

    Add existing lobby database schema to config

---
+++ config/lobby.sql
@@ -0,0 +1,52 @@
+CREATE TABLE chatMessages (
+			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			senderName TEXT,
+			channelType TEXT,
+			channelName TEXT,
+			messageText TEXT,
+			creationTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+		);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE gameRoomPlayers (
+			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			roomID TEXT,
+			accountID TEXT
+		);
+CREATE TABLE gameRooms (
+			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			roomID TEXT,
+			hostAccountID TEXT,
+			description TEXT NOT NULL DEFAULT '',
+			status INTEGER NOT NULL DEFAULT 0,
+			playerLimit INTEGER NOT NULL,
+			creationTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+		, mods TEXT NOT NULL DEFAULT '{}', version TEXT NOT NULL DEFAULT '');
+CREATE TABLE accounts (
+			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			accountID TEXT,
+			displayName TEXT,
+			online INTEGER NOT NULL,
+			lastLoginTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+			creationTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+		);
+CREATE TABLE accountCookies (
+			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			accountID TEXT,
+			cookieUUID TEXT,
+			creationTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+		);
+CREATE TABLE gameRoomInvites (
+			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			roomID TEXT,
+			accountID TEXT
+		);
+CREATE TABLE sqlite_stat1(tbl,idx,stat);
+CREATE INDEX idx_accounts_online ON accounts(online);
+CREATE INDEX idx_gameRooms_status ON gameRooms(status);
+CREATE INDEX idx_chatMessages_creationTime ON chatMessages(creationTime);
+CREATE INDEX idx_chatMessages_channelTypeName ON chatMessages(channelType, channelName);
+CREATE INDEX idx_gameRooomPlayers_account ON gameRoomPlayers(accountID);
+CREATE INDEX gameRoomPlayersAccountID ON gameRoomPlayers(accountID);
+CREATE INDEX gameRooms_roomID ON gameRooms(roomID);
+CREATE INDEX accounts_accountID ON accounts(accountID);
+CREATE INDEX gameRoomPlayers_roomID ON gameRoomPlayers(roomID);
